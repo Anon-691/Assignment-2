8757d1e0e280d473b8cbc90af98350a695b35548
Anon-691 <a.adeel2027@gmail.com>
2026-03-28 08:01:09 +0500

    IntToBinary_PartA

--- IntToBinary_PartA.java
+++ IntToBinary_PartA.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IntToBinary {
+public class IntToBinary_PartA {
     public static  void main(String[] args)
    { Scanner sc=new Scanner(System.in);
      System.out.println("Enter an integer number:");
